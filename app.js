71f70fd5fae6724c7b1a1658849105bfca054881
Dan Stroot <dan.stroot@gmail.com>
2013-12-20 13:23:05 -0800

    Added License

--- app.js
+++ app.js
@@ -1,16 +1,9 @@
-
-/**
- * Module dependencies.
- */
-
 var express         = require('express');
 var http            = require('http');
 var path            = require('path');
 var lessMiddleware  = require('less-middleware');    // https://npmjs.org/package/less-middleware
 
 var app = express();
-
-// all environments
 app.set('port', process.env.PORT || 3000);
 
 /* =================================================
@@ -24,21 +17,17 @@ app.use(lessMiddleware({
  , debug: false
 }));
 
-app.use(express.favicon());
+/* =================================================
+ Set it up (Middleware)
+=================================================== */
+//app.use(express.favicon());
 app.use(express.logger('dev'));
-// app.use(express.json());
-// app.use(express.urlencoded());
-// app.use(express.methodOverride());
-// app.use(express.cookieParser('your secret here'));
-// app.use(express.session());
-// app.use(app.router);
 app.use(express.static(path.join(__dirname, '')));
+app.use(express.errorHandler());
 
-// development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}
-
+/* =================================================
+ Start it up!
+=================================================== */
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
